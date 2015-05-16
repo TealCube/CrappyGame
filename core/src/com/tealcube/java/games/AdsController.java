dfcec44d048dfeaa0936303ec3218aa637c1f057
Kyle Wolfgang Kattrein <kattreinkyle@gmail.com>
2015-05-16 00:42:38 -0400

    ads

--- core/src/com/tealcube/java/games/AdsController.java
+++ core/src/com/tealcube/java/games/AdsController.java
@@ -3,4 +3,5 @@ package com.tealcube.java.games;
 public interface AdsController {
     public boolean isWifiConnected();
     public void showInterstitialAd (Runnable then);
+    // vfrgvcdfrgbfd
 }
