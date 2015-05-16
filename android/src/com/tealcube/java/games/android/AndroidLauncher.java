bdf8e028d265edc192e9aa696d0978774bc8e9bc
Richard Harrah <topplethenunnery@gmail.com>
2015-05-16 01:21:28 -0400

    optimizing imports and fixing sourceCompatibility

--- android/src/com/tealcube/java/games/android/AndroidLauncher.java
+++ android/src/com/tealcube/java/games/android/AndroidLauncher.java
@@ -4,22 +4,13 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
-import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
-
 import com.tealcube.java.games.AdsController;
-import com.tealcube.java.games.CrappyGame;
 
 public class AndroidLauncher extends AndroidApplication implements AdsController {
 
@@ -34,7 +25,7 @@ public class AndroidLauncher extends AndroidApplication implements AdsController
     InterstitialAd interstitialAd;
 
     @Override
-    protected void onCreate (Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
     }
