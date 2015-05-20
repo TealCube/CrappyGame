38dbcdf957ae7095c68323d301f5d66e6fbd28b1
Kyle Wolfgang Kattrein <kattreinkyle@gmail.com>
2015-05-19 23:38:37 -0400

    Adding apple garbage... Ugh... apple....

--- ios/src/com/tealcube/java/games/IOSLauncher.java
+++ ios/src/com/tealcube/java/games/IOSLauncher.java
@@ -5,7 +5,7 @@ import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.tealcube.java.games.CrappyGame;
+import com.tealcube.java.games.TacirGame;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
